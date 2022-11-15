c3cc39744053603c47aea12ab5177ed25ed2001f
LcSilva0702 <lcsilva0702@gmail.com>
2022-11-15 15:28:12 -0300

    Programa bootcamp feito com sucesso

--- Abstraindo um projeto/src/Main.java
+++ Abstraindo um projeto/src/Main.java
@@ -1,6 +1,9 @@
 import java.time.LocalDate;
 
+import br.dio.com.desafio.dominio.Bootcamp;
+import br.dio.com.desafio.dominio.Conteudo;
 import br.dio.com.desafio.dominio.Curso;
+import br.dio.com.desafio.dominio.Dev;
 import br.dio.com.desafio.dominio.Mentoria;
 
 public class Main {
@@ -14,5 +17,34 @@ public class Main {
         curso1.setTitulo("Curso de Java");
 
         mentoria1.setData(LocalDate.now());
+        mentoria1.setTitulo("Bootcamp java");
+        mentoria1.setDescricao("Bootcamp Ifood");
+
+        Bootcamp bootcampJavaIfood = new Bootcamp();
+
+        bootcampJavaIfood.setNome("Bootcamp Java Potencia Tech");
+        bootcampJavaIfood.getConteudos().add(curso1);
+        bootcampJavaIfood.getConteudos().add(mentoria1);
+        bootcampJavaIfood.setDescricao("Descricao bootcamp");
+
+        Dev dev1 = new Dev();
+        dev1.inscreverBootcamp(bootcampJavaIfood);
+        dev1.setNome("Lucas");
+
+        System.out.println("Conteudo Inscritos: " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        System.out.println("Conteudo Inscritos: " + dev1.getConteudosInscritos());
+        System.out.println("Conteudo Inscritos: " + dev1.getConteudosConcluidos());
+        System.out.println("Xp: " + dev1.calcularTotalXp());
+
+        Dev dev2 = new Dev();
+        dev2.inscreverBootcamp(bootcampJavaIfood);
+        dev2.setNome("Claudinete");
+
+        System.out.println("Conteudo Inscritos: " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("Conteudo Inscritos: " + dev2.getConteudosInscritos());
+        System.out.println("Conteudo Inscritos: " + dev2.getConteudosConcluidos());
+        System.out.println("Xp: " + dev2.calcularTotalXp());
     }
 }
